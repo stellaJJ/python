f4c7270b1f21ff9ab6410bc5bcbb92e80711d538
stellaJJ <121223615+stellaJJ@users.noreply.github.com>
2024-04-26 17:11:30 +0900

    Add files via upload

---
+++ def do_one_set(n)_for문_7주차:.py
@@ -0,0 +1,19 @@
+'''
+def do_one_set(n):
+    for cnt in range(1,n+1):
+        print(cnt,end=' ')
+    print()
+
+for s in range(3):
+    do_one_set(15)
+'''
+
+def do_one_set(n):
+    for cnt in range(1,n+1):
+        print(cnt,end=' ')
+    print()
+
+n=15
+for s in range(3):
+    do_one_set(n)
+
