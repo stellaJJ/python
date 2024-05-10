ea9c21bda8ecd13a6ed32270578d92b5c980492c
stellaJJ <121223615+stellaJJ@users.noreply.github.com>
2024-05-10 11:11:40 +0900

    Delete hw7_9주차_장바구니담기.py

--- hw7_9주차_장바구니담기.py
+++
@@ -1,9 +0,0 @@
-shopping_bag=[]
-print('[구입]')
-while True:
-    item=input('상품명?')
-    print(f'장바구니에 {item}이(가) 담겼습니다.')
-    shopping_bag.append(item)
-    if item=='':
-        break
-print(f'장바구니 보기: {shopping_bag}')
