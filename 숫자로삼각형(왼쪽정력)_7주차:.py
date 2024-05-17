abecafaee0de6bfe89ae2e31e99260462db9e80c
stellaJJ <121223615+stellaJJ@users.noreply.github.com>
2024-05-17 19:52:52 +0900

    Delete 숫자로삼각형(왼쪽정력)_7주차:.py

--- 숫자로삼각형(왼쪽정력)_7주차:.py
+++
@@ -1,8 +0,0 @@
-def display_triangle(height):
-    for i in range(1, height+1):
-        for j in range(1, i+1):
-            print(j,end='')
-        print()
-
-h=int(input('높이?'))
-display_triangle(h) 
