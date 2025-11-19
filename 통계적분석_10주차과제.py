d0607eb94a6f3fa840677bd3c63605a11cd62451
stellaJJ <121223615+stellaJJ@users.noreply.github.com>
2025-11-19 23:48:18 +0900

    Add files via upload

---
+++ 통계적분석_10주차과제.py
@@ -0,0 +1,253 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[42]:
+
+
+import numpy as np
+from scipy import stats
+import matplotlib.pyplot as plt
+get_ipython().run_line_magic('precision', '3')
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# In[45]:
+
+
+linestyles =['-', '--', ':']
+def E(X, g= lambda x: x):
+    x_set, f= X
+    return np.sum([g(x_k) * f(x_k) for x_k in x_set])
+
+def V(X, g= lambda x: x):
+    x_set, f=X
+    mean=E(X, g)
+    return np.sum([(g(x_k)-mean) **2 * f(x_k) for x_k in x_set])
+
+def check_prob(X):
+    x_set, f=X
+    prob = np.array([f(x_k) for x_k in x_set])
+    assert np.all(prob>=0), 'minus probability'
+    prob_sum= np.round(np.sum(prob), 6)
+    assert prob_sum == 1, f'sum of probability {prob_sum}'
+    print(f'expected value {E(X):.4}')
+    print(f'variance {V(X):.4}')
+
+def plot_prob(X):
+    x_set, f=X
+    prob= np.array([f(x_k) for x_k in x_set])
+
+    fig= plt.figure(figsize=(10,6))
+    ax= fig.add_subplot(111)
+    ax.bar(x_set, prob, label='prob')
+    ax.set_xticks(np.append(x_set, E(X)))
+    ax.set_ylim(0, prob.max()*1.2)
+    ax.legend()
+
+    plt.show()
+    
+
+
+# In[46]:
+
+
+#베르누이 분포
+def Bern(p):
+    x_set = np.array([0,1])
+    def f(x):
+        if x in x_set:
+            return p** x* (1-p) **(1-x)
+        else:
+            return 0
+
+    return x_set, f
+
+p=0.3
+X=Bern(p)
+
+
+# In[47]:
+
+
+check_prob(X)
+
+
+# In[48]:
+
+
+rv=stats.bernoulli(p)
+
+
+# In[49]:
+
+
+rv.pmf(0), rv.pmf(1)
+
+
+# In[50]:
+
+
+rv.pmf([0,1])
+
+
+# In[51]:
+
+
+rv.cdf([0,1])
+
+
+# In[52]:
+
+
+rv.mean(), rv.var()
+
+
+# In[53]:
+
+
+from scipy.special import comb
+def Bin(n,p):
+    x_set = np.arange(n+1)
+    def f(x):
+        if x in x_set:
+            return comb(n,x) * p**x * (1-p) **(n-x)
+        else:
+            return 0
+
+    return x_set, f
+
+
+n=10
+p=0.3
+X=Bin(n,p)
+    
+
+
+# In[54]:
+
+
+check_prob(X)
+
+
+# In[55]:
+
+
+plot_prob(X)
+
+
+# In[57]:
+
+
+fig = plt.figure(figsize=(10,6))
+ax = fig.add_subplot(111)
+
+x_set= np.arange(n+1)
+for p, ls in zip([0.3, 0.5, 0.7], linestyles):
+    rv= stats.binom(n,p)
+    ax.plot(x_set, rv.pmf(x_set),
+            label= f'p:{p}', ls=ls, color='gray')
+ax.set_xticks(x_set)
+ax.legend()
+
+plt.show()
+
+
+# In[64]:
+
+
+def Ge(p):
+    x_set= np.arange(1,30)
+    def f(x):
+        if x in x_set:
+            return p*(1-p) **(x-1)
+        else:
+            return 0
+
+    return x_set, f
+
+p=0.5
+X=Ge(p)
+
+
+# In[65]:
+
+
+check_prob(X)
+
+
+# In[66]:
+
+
+plot_prob(X)
+
+
+# In[67]:
+
+
+fig = plt.figure(figsize=(10,6))
+ax = fig.add_subplot(111)
+
+x_set= np.arange(1,15)
+for p, ls in zip([0.2, 0.5, 0.8], linestyles):
+    rv= stats.geom(p)
+    ax.plot(x_set, rv.pmf(x_set),
+            label= f'p:{p}', ls=ls, color='gray')
+ax.set_xticks(x_set)
+ax.legend()
+
+plt.show()
+
+
+# In[78]:
+
+
+from scipy.special import factorial
+
+def Poi(lam):
+    x_set= np.arange(20)
+    def f(x):
+        if x in x_set:
+            return np.power(lam, x) / factorial(x) *np.exp(-lam)
+
+        else:
+            return 0
+    return x_set, f
+
+lam=3
+X=Poi(lam)
+
+
+# In[79]:
+
+
+check_prob(X)
+
+
+# In[80]:
+
+
+plot_prob(X)
+
+
+# In[82]:
+
+
+fig = plt.figure(figsize=(10,6))
+ax = fig.add_subplot(111)
+
+x_set= np.arange(20)
+for lam, ls in zip([3, 5, 8], linestyles):
+    rv= stats.poisson(lam)
+    ax.plot(x_set, rv.pmf(x_set),
+            label= f'p:{p}', ls=ls, color='gray')
+ax.set_xticks(x_set)
+ax.legend()
+
+plt.show()
+
+
+# In[ ]:
+
+
+
+
